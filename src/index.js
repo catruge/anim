4d664c9a5bf72d7c3bda9e5cf88ca82dbb0c2fe8
catruge <e7rtzb5c1@relay.firefox.com>
2021-03-15 16:48:48 -0400

    diffEqTri optional arguments

--- src/index.js
+++ src/index.js
@@ -2358,18 +2358,7 @@ math.import({
         }
         rtv.ctx.stroke();
     },
-    diffEqTri(a, b, c, d, x0, y0, yp0, ypp0, _n, _dt) { // ay''' + by'' + cy' + dy = 0 numerically plotted for _n steps and _dt accuracy
-        let n = 1000;
-        let dt = .001;
-
-        if (arguments.length >= 8) {
-            n = _n;
-        }
-
-        if (arguments.length >= 9) {
-            dt = _dt;
-        }
-
+    diffEqTri(a, b, c, d, x0, y0, yp0, ypp0, n = 1000, dt = 0.001) { // ay''' + by'' + cy' + dy = 0 numerically plotted for _n steps and _dt accuracy
         let y = y0;
         let x = x0;
         let yp = yp0;
