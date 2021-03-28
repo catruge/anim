2735d78ca09650bb4cd675fdbea551ad89753ef8
catruge <e7rtzb5c1@relay.firefox.com>
2021-03-28 14:07:02 -0400

    Fix no-nested-ternary

--- src/index.js
+++ src/index.js
@@ -1786,7 +1786,7 @@ math.import({
 
       indices
         .map((i) => camData[i][2])
-        .sort((a, b) => (a < b ? 1 : (a > b ? -1 : 1)));
+        .sort((a, b) => (a <= b ? 1 : -1));
 
       let col;
       for (let j = 0; j < n; j++) {
